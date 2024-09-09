ad4411098d0f9514a37f3de7ff4afd9427335de0
kelly9396 <kellyvisagie93@gmail.com>
2024-09-09 16:21:51 +0200

    Added AI integrations and axios

--- generator/javascript.js
+++ generator/javascript.js
@@ -1,12 +1,23 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function showPoem(response) {
   new Typewriter("#poem", {
-    strings: "Gentle winds, Flowers bend, Petals dance, Nature's trance",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: null,
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructions = document.querySelector("#instructions");
+  let apiKey = "of08ta1f90c49d4b081b6aa347d98489";
+  let prompt = `Generate any poem that the ${instructions.value}`;
+  let context =
+    "You are an expert in writing short and sweet poems. Generate a 5 line poem in basic HTML and separate each line with a <br />. Don't display the title and html. Sign the poem with Kelly Visagie❤️ in <em>";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(showPoem);
 }
 
 let poemForm = document.querySelector("#poem-generator");
